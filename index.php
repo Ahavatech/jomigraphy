5f3bbcef031a2b652cddbca21ed63a787fddfe42
Ibukunoluwa Adekunle <82944318+ibkclicks@users.noreply.github.com>
2022-09-12 15:03:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,13 +41,13 @@
 <div id="nav">
   <nav class="navbar navbar-custom">
     <div class="container">
+<a class="navbar-brand" href="#">JomiGraphy</a>
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> <i class="fa fa-bars"></i> </button>
       </div>
       
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse navbar-main-collapse">
-        <a class="navbar-brand" href="#">JomiGraphy</a>
         <ul class="nav navbar-nav">
           <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
           <li class="hidden"> <a href="#page-top"></a> </li>
